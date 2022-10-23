10e593487db0f2d8533329c17d9d923e63af837d
Levon Poghosyan <levon9393@bk.ru>
2022-10-23 09:21:02 +0300

    Some changes

--- src/test/java/ru/netology/cucumberSteps/TransferSteps.java
+++ src/test/java/ru/netology/cucumberSteps/TransferSteps.java
@@ -18,40 +18,24 @@ public class TransferSteps {
     public void auth(String login, String password) {
         open("http://localhost:9999/");
         loginPage = new LoginPage();
-        var verifyPage = loginPage.validLogin(login, password);
-    }
-
-    /*@Пусть("открыта страница с формой авторизации {string}")
-    public void openAuthPage(String url) {
-        loginPage = open(url, LoginPage.class);
-    }
-
-
-    @Когда("пользователь пытается авторизоваться с именем {string} и паролем {string}")
-    public void loginWithNameAndPassword(String login, String password) {
         verifyPage = loginPage.validLogin(login, password);
-    }*/
+    }
 
     @И("c проверочным кодом {string}")
     public void setValidCode(String verifyCode) {
-        verifyPage = new VerificationPage();
         dashboard = verifyPage.verify(verifyCode);
     }
 
-    @Когда("пользователь переводит {string} рублей с карты с номером {string} на свою {int} карту с главной страницы")
-    public void successTransfer(String amount, String cardFrom, int indexCardTo) {
-        var transferPage = dashboard.transferClick(indexCardTo - 1);
+    @Когда("пользователь переводит {string} рублей с карты с номером {string} на свою {string} карту")
+    public void successTransfer(String amount, String cardFrom, String indexCardTo) {
+        var transferPage = dashboard.transferClick(Integer.valueOf(indexCardTo) - 1);
         transferPage.importTransferData(amount, cardFrom);
     }
 
     @Тогда("баланс его {string} карты из списка на главной странице должен стать {string} рублей")
     public void matchesBalance(String indexCard, String expectedBalance) {
         dashboard.reloadBalance();
-        String[] balance = expectedBalance.split(" ");
-        String sum = "";
-        for (String tmp : balance) {
-            sum = sum + tmp;
-        }
+        var sum = expectedBalance.replaceAll(" ","");
         int actualBalance = dashboard.getBalance(Integer.valueOf(indexCard) - 1);
         assertEquals(Integer.valueOf(sum), actualBalance);
     }
